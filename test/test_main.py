a85d7703d96e7e0b7be505c5f1d62e4f51eb8c41
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2023-05-08 18:04:10 -0300

    Reduce the number of errors while running pylint, flake8 and bandit

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""
+Unit test for Todowarrior.main
+
+Usage: pylint -v
+
+For more information, se the file README.md
+"""
 
 import os
 import unittest
@@ -8,16 +15,20 @@ from todowarrior.main import get_access_token_from_login
 
 
 class TestMain(unittest.TestCase):
+    """
+    This is the only testclass for this project
+    """
 
     @mock.patch.dict(os.environ, {"TOODLEDO_CLIENT": "client_id:secret"},
                      clear=True)
-    @mock.patch('random.choices', return_value=list('12345678'))
-    @mock.patch('builtins.input',
-                return_value='https://example.com/?code=abcdefg&state=12345678'
-                )
+    @mock.patch('secrets.choice')
+    @mock.patch('builtins.input')
     @mock.patch('requests.post')
     def test_get_access_token_from_login(self, mock_post, mock_input,
-                                         mock_random_choices):
+                                         mock_secrets_choice):
+        mock_secrets_choice.side_effect = list('12345678')
+        mock_input.return_value = (
+            'https://example.com/?code=abcdefg&state=12345678')
         mock_post.return_value.text = '{"access_token": "my_access_token"}'
         result = get_access_token_from_login()
         mock_post.assert_called_once_with(
@@ -29,5 +40,6 @@ class TestMain(unittest.TestCase):
             headers={
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Authorization': 'Basic Y2xpZW50X2lkOnNlY3JldA=='
-            })
+            },
+            timeout=30)
         self.assertEqual(result, 'my_access_token')
--- todowarrior/main.py
+++ todowarrior/main.py
@@ -1,45 +1,57 @@
 #!/usr/bin/env python
+"""
+Todowarrior - Synchronize Toodledo and Taskwarrior
+
+This file doesn't include a "usage" because it's still not usable.
+
+For more information, see the file README.md.
+"""
 
 import base64
 import json
 import os
-import random
-import requests
+import secrets
 import string
 import sys
 import urllib.parse
 from urllib.parse import urlparse, parse_qs
 
+import requests
+
+REQUESTS_TIMEOUT_SECONDS = 30
+
 
 def get_access_token_from_login():
     # Toodledo OAuth2 settings
     toodledo_client_envvar = os.getenv('TOODLEDO_CLIENT')
-    TOODLEDO_CLIENT_ID, TOODLEDO_CLIENT_SECRET = toodledo_client_envvar.split(
+    toodledo_client_id, toodledo_client_secret = toodledo_client_envvar.split(
         ':')
 
     random_string = ''.join(
-        random.choices(string.ascii_uppercase + string.digits, k=8))
+        secrets.choice(string.ascii_uppercase + string.digits)
+        for _ in range(8))
 
     # Generate the authorization URL and prompt the user to authorize the app
-    site = (f'https://api.toodledo.com/3/account/authorize.php' +
-            f'?response_type=code&client_id={TOODLEDO_CLIENT_ID}' +
+    site = ('https://api.toodledo.com/3/account/authorize.php' +
+            f'?response_type=code&client_id={toodledo_client_id}' +
             f'&state={random_string}&scope=basic%20tasks')
     print('Please go to this URL to authorize the application:', site)
 
-    # Get the authorization code from the user and exchange it for an access token
+    # Get the authorization code from the user
     authorization_code_full_uri = input(
         '\nEnter the authorization code from the website: ')
 
     query = parse_qs(urlparse(authorization_code_full_uri).query)
-    if query['state'][0] != random_string:
+    if secrets.compare_digest(query['state'][0], random_string) is False:
         sys.exit('Error in URI')
 
     authorization_code = query['code'][0]
 
-    client_id = urllib.parse.quote(TOODLEDO_CLIENT_ID.encode('utf8'))
-    clientSecret = urllib.parse.quote(TOODLEDO_CLIENT_SECRET.encode('utf8'))
+    # Exchange the authorization code for an access token
+    client_id = urllib.parse.quote(toodledo_client_id.encode('utf8'))
+    client_secret = urllib.parse.quote(toodledo_client_secret.encode('utf8'))
 
-    code_bytes = f"{client_id}:{clientSecret}".encode('ascii')
+    code_bytes = f"{client_id}:{client_secret}".encode('ascii')
     base64_bytes = base64.b64encode(code_bytes)
     base64_code = base64_bytes.decode('ascii')
 
@@ -55,20 +67,23 @@ def get_access_token_from_login():
         'code': authorization_code,
     }
 
-    response = requests.post(url, data=data, headers=headers)
+    response = requests.post(url,
+                             data=data,
+                             headers=headers,
+                             timeout=REQUESTS_TIMEOUT_SECONDS)
     access_token = json.loads(response.text)['access_token']
     # TODO: use refresh token
     return access_token
 
 
-def main(argv):
+def main(_argv):
     access_token = get_access_token_from_login()
 
     print('\nGetting data')
     url = ('http://api.toodledo.com/3/tasks/get.php' +
            f'?access_token={access_token}')
 
-    response = requests.post(url)
+    response = requests.post(url, timeout=REQUESTS_TIMEOUT_SECONDS)
     print(response)
     print(response.text)
     print()
